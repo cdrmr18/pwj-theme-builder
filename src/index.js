3a92944c0212b7f31c4ff2ce66a0d312efd96d07
Kathy Tavia <kathy.tavia@gmail.com>
2021-08-01 23:28:33 +0200

    added global styles file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import * as themes from './themes/schema.json';
+import * as themes from './theme/schema.json';
 import { setToLS } from './utils/storage';
 
 const Index = () => {
